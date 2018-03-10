1a99cb554b04e8a93159122be22a7a715b81e5d1
Tomoka Baba <baba@tofulab.jp>
2018-03-10 16:28:28 +0900

    postcss-nestedとpostcss-simple-varsを追加

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: [
+    require('postcss-simple-vars')(),
+    require('postcss-nested')(),
+    require('postcss-normalize')(),
     require('autoprefixer')(),
     require('postcss-normalize')(),
     require('cssnano')({
